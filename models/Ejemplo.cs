5aaf7c4535dff4a2374cad39124a980213657443
val <valeria@gmail.com>
2024-07-22 06:40:35 -0500

    Se creo la clase ejemplo

---
+++ models/Ejemplo.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POO_static.models
+{
+    public class Ejemplo
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+    }
+
+    public Ejemplo(){
+        Nombre="Valeria";
+        Apellido="cano";
+    }
+
+    public void Saludar(){
+        Console.WriteLine($"Hola {Nombre} {Apellido}");
+    }
+}
